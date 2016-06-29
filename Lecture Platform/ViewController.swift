4c41d77abfeef8981fb79aa4f51eca94919662ce
Abhishek Bhattacharya <abhi.intelled@gmail.com>
2016-06-29 20:02:40 +0530

    Initial Commit

---
+++ Lecture Platform/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lecture Platform
+//
+//  Created by Abhishek Bhattacharya on 29/06/16.
+//  Copyright © 2016 Abhishek Bhattacharya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
